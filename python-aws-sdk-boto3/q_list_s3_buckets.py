7a8050a36506bcdf780e822907d38e6feacdbaeb
1d9akash <d19akash@gmail.com>
2025-04-06 18:54:22 +0530

    Added & Updated a few scripts of Botot3 with Error Handling | ListInstances, ListS3Buckets, VerifyBoto3Installation

--- python-aws-sdk-boto3/q_list_s3_buckets.py
+++ python-aws-sdk-boto3/q_list_s3_buckets.py
@@ -1,7 +1,18 @@
 import boto3
-client = boto3.client('s3')
-response = client.list_buckets()
-i = 1
-for bucket in response['Buckets']:
-    print(f"- Bucket {i}: {bucket['Name']}")
-    i += 1
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError, ClientError
+
+try:
+    client = boto3.client('s3')
+    response = client.list_buckets()
+    i = 1
+    for bucket in response['Buckets']:
+        print(f"- Bucket {i}: {bucket['Name']}")
+        i += 1
+except NoCredentialsError as e:
+    print(f"No AWS Credentials found | {e}")
+except PartialCredentialsError as e:
+    print(f"Incomplete AWS Credentials | {e}")
+except ClientError as e:
+    print(f"AWS Client Error occured | {e}")
+except Exception as e:
+    print(f"Error: {e}")
