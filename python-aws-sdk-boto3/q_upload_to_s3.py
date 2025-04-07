95224275de820d27323876d21625c384c5b805ba
1d9akash <d19akash@gmail.com>
2025-04-07 16:56:30 +0530

    Updated UploadToS3 Script with ListS3Objects

--- python-aws-sdk-boto3/q_upload_to_s3.py
+++ python-aws-sdk-boto3/q_upload_to_s3.py
@@ -5,17 +5,23 @@ s3 = boto3.client('s3')
 
 def upload_to_s3(bucket_name, file_path, s3_key):
     return s3.upload_file(file_path, bucket_name, s3_key)
-# def list_s3_objects(bucket_name):
-#     return s3.list_objects_v2(Bucket=bucket_name)
+def list_s3_objects(bucket_name):
+    paginator = s3.get_paginator('list_objects_v2')
+    response_iterator = paginator.paginate(Bucket=bucket_name)
+    print(f"Files In{bucket_name}:")
+    for page in response_iterator:
+        if 'Contents' in page:
+            for obj in page['Contents']:
+                file = obj.get('Key', 'N/A')
+                print(f"- {file}")
 try:
     bucket_name = input("Enter the Bucket Name: ").strip()
     file_path = input("Enter the File Name ( Ex: test.txt ): ").strip()
     s3_key = input("Enter the Key (Ex: uploads/test.txt ): ").strip()
     upload_to_s3(bucket_name, file_path, s3_key)
     print(f"File {file_path} uploaded successfully to s3://{bucket_name} Bucket")
-#    result = s3.list_objects_v2(Bucket=bucket_name)
-#    with open("sample-s3-listobjects.txt", "w") as f:
-#        print(result,file=f)
+    list_s3_objects(bucket_name)
+
 except NoCredentialsError as e:
     print(f"No AWS Credentials found | {e}")
 except PartialCredentialsError as e:
