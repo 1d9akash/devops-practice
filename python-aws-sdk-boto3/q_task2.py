5f9e15ce303a5ee5325ac9c92673178cf29a4889
1d9akash <d19akash@gmail.com>
2025-04-10 22:42:23 +0530

    Added Task-3 as python-aws-sdk-boto3/q_task3.py | Updated q_task1.py and q_task2.py

--- python-aws-sdk-boto3/q_task2.py
+++ python-aws-sdk-boto3/q_task2.py
@@ -24,26 +24,26 @@ def upload_to_s3(s3, bucket_name, file_path, s3_key):
         print(f"Error: The file '{file_path}' was not found.")
     except Exception as e:
         print(f"Error uploading file: {e}")
-        
-try: 
-    region = input("Enter the region: ").strip()
-    s3 = s3_client(region)
-    bucket_name = input("Enter the Bucket Name: ").strip()
-    file_path = input("Enter the File Name ( Ex: test.txt ): ").strip()
-    s3_key = input("Enter the Key (Ex: uploads/test.txt ): ").strip()
-    tag = upload_to_s3(s3, bucket_name, file_path, s3_key)
 
-    if tag:
-        print(f"File '{file_path}' uploaded with tag {tag}")
-    else:
-        print(f"File '{file_path}' uploaded without any tag")
+if __name__=='__main__':
+    try: 
+        region = input("Enter the region: ").strip()
+        s3 = s3_client(region)
+        bucket_name = input("Enter the Bucket Name: ").strip()
+        file_path = input("Enter the File Name ( Ex: test.txt ): ").strip()
+        s3_key = input("Enter the Key (Ex: uploads/test.txt ): ").strip()
+        tag = upload_to_s3(s3, bucket_name, file_path, s3_key)
 
+        if tag:
+            print(f"File '{file_path}' uploaded with tag {tag}")
+        else:
+            print(f"File '{file_path}' uploaded without any tag")
 
-except NoCredentialsError as e:
-    print(f"No AWS Credentials found | {e}")
-except PartialCredentialsError as e:
-    print(f"Incomplete AWS Credentials | {e}")
-except ClientError as e:
-    print(f"AWS Client Error occured | {e}")
-except Exception as e:
-    print(f"Error: {e}")
+    except NoCredentialsError as e:
+        print(f"No AWS Credentials found | {e}")
+    except PartialCredentialsError as e:
+        print(f"Incomplete AWS Credentials | {e}")
+    except ClientError as e:
+        print(f"AWS Client Error occured | {e}")
+    except Exception as e:
+        print(f"Error: {e}")
